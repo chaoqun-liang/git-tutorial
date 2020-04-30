1aecfab02fbb45044e06bbae259eb5b27ece6438
chaoqun-liang <c.liang@tum.de>
2020-04-30 23:14:38 +0200

    Completed coding challenge 2

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <espl_lib.h>
+
 int main() {
-    printf("Hello ESPL");
-    return 0;
+    int input_num;
+    char ans;
+
+    do{
+    printf("Please enter a number:\n"); 
+    scanf("%d",&input_num);
+    char *returned_str = num_to_words(input_num);
+    printf("The corresponding word is : %s \n",returned_str);
+    printf("Do your want to run another number[Y/N]: ");
+    scanf(" %c",&ans);
+    }while(ans == 'Y' || ans == 'y');
 }
